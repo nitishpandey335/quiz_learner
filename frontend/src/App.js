d5587d9dd04d537b82ed81bf947255c9f09453fc
nitishpandey335 <nitishkumarpandey05@gmail.com>
2026-05-08 01:23:52 +0530

    Fix build: remove unused import, disable sourcemap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider, useAuth } from './context/AuthContext';
